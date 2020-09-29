207dc37e60d598c82b510d64e7e649469a3d9b11
lightkuriboh <phamngochieu210199@gmail.com>
2020-09-29 20:49:44 +0700

    add slide

--- Final Project/ui.R
+++ Final Project/ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage(
             tabsetPanel(type='tabs',
                         tabPanel('Column plot', br(), plotOutput('output_column')),
                         tabPanel('Line plot', br(), plotOutput('output_line')),
-                        tabPanel('Instruction', br(), div('Instruction: Use the sliders to modify parameters, which are use to randomly generate sample mean, which centers at the population means. You can see the effects of the number of simulations, as well as the size of sample on the theorem. With this, you can easily see what the Central Limit Theorem is.'))
+                        tabPanel('Document', br(), div('Instruction: Use the sliders to modify parameters, which are use to randomly generate sample mean, which centers at the population means. You can see the effects of the number of simulations, as well as the size of sample on the theorem. With this, you can easily see what the Central Limit Theorem is.'))
             )
         ),
         sidebarPanel(
